703d89cdaa4d7b721325b91ea8a4362459986dbc
Hayden <haybartlett@gmail.com>
2018-01-06 13:30:26 +1030

    Display syntax highlighted component code from file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  componentHighlightedMarkup = require('!!prismjs-loader?lang=markup!./app.component.html');
 }
